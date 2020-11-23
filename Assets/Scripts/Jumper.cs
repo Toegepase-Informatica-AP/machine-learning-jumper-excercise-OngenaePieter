11aeac4b1c3abd79c996823d5fec878783fe0d3a
OngenaePieter <pieterbracke@hotmail.com>
2020-11-23 11:59:53 +0100

    Scene, Player, Scripts
    Edited the scene
    Added scripts
    Added player

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class Jumper : Agent
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private KeyCode jumpKey;
+    
+    private bool jumpIsReady = true;
+    private Rigidbody rBody;
+    private Vector3 startingPosition;
+    
+    public event Action OnReset;
+    
+    public override void Initialize()
+    {
+        rBody = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if(jumpIsReady)
+            RequestDecision();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.FloorToInt(vectorAction[0]) == 1)
+            Jump();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        Reset();
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
+        
+        if (Input.GetKey(jumpKey))
+            actionsOut[0] = 1;
+    }
+
+    private void Jump()
+    {
+        if (jumpIsReady)
+        {
+            rBody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
+            jumpIsReady = false;
+        }
+    }
+    
+    private void Reset()
+    {
+        jumpIsReady = true;
+        transform.position = startingPosition;
+        rBody.velocity = Vector3.zero;
+        
+        OnReset?.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision collidedObj)
+    {
+        if (collidedObj.gameObject.CompareTag("Street"))
+            jumpIsReady = true;
+        
+        else if (collidedObj.gameObject.CompareTag("Obstacle"))
+        {
+            AddReward(-1.0f);
+            EndEpisode();
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject Obstacle;
+    [SerializeField] private float minSpawnIntervalInSeconds;
+    [SerializeField] private float maxSpawnIntervalInSeconds;
+
+    private Jumper jumper;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        jumper = GetComponentInChildren<Jumper>();
+        jumper.OnReset += DestroyAllSpawnedObjects;
+        
+        StartCoroutine(nameof(Spawn));
+    }
+    
+    private IEnumerator Spawn()
+    {
+        var spawned = Instantiate(Obstacle, transform.position, transform.rotation, transform);
+        spawnedObjects.Add(spawned);
+        
+        yield return new WaitForSeconds(Random.Range(minSpawnIntervalInSeconds, maxSpawnIntervalInSeconds));
+        StartCoroutine(nameof(Spawn));
+    }
+    private void DestroyAllSpawnedObjects()
+    {
+        for (int i = spawnedObjects.Count - 1; i >= 0; i--)
+        {
+            Destroy(spawnedObjects[i]);
+            spawnedObjects.RemoveAt(i);
+        }
+    }
+}
